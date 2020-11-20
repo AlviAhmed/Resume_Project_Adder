cdb93f7393db0ccf3460f1aab185f32058183437
AlviAhmed <alviahmed1234@gmail.com>
2020-11-20 13:04:44 -0500

    Log1

---
+++ mainScript.sh
@@ -0,0 +1,27 @@
+#!/bin/bash 
+. bashFunc.sh                   # header files that contains the awk function
+
+trap cleanupFunc EXIT
+
+
+while :
+
+do      
+
+    read -p "Enter Skill: " userinp
+    echo ""
+    awkFunc 
+    sedFunc
+    numLines=$(grep $userinp buffer.tex | wc -l) 
+
+    if [ "${numLines}" -eq "0" ];
+    then
+        echo "no projects exist for skill: $userinp"
+    else
+        echo "${numLines} projects for skill: $userinp  "
+    fi
+
+    echo "To exit script press <CTRL+C>"
+
+done
+
