e76e30fe2043c112abb1cd5ad21cab8092989263
AlviAhmed <alviahmed1234@gmail.com>
2020-11-25 16:09:11 -0500

    added comments, problems with returning to loop

--- bashFunc.sh
+++ bashFunc.sh
@@ -1,65 +1,130 @@
 #!/bin/bash
 
 
-resStoreCheckFunc(){
-    DIR="resume_storage"
-    if [ -d "$DIR" ];
+resStoreCheckFunc(){            # this function is in charge of creating the main resume storage and checking its existence
+    
+    DIR="resume_storage"        # creating a generic directory to store all generated resumes
+    if [ -d "$DIR" ];           # if directory exists
     then
         printf "Directory already exists....clear \n"
-    else
+    else                        # if directory doesn't exist, make one
         printf "Directory DOES NOT exist making one right now... \n"
         mkdir $DIR
     fi
+
 }
 
-resDirFunc(){
-    comp=$(echo $comp | sed -e "s/ /\_/g")
+resDirFunc(){                   # this function deals with making the directories and files for the new resumes
+    
+    comp=$(echo $comp | sed -e "s/ /\_/g") # replacing spaces with underscores in the variables comp, pos, and name
     pos=$(echo $pos | sed -e "s/ /\_/g")
     name=$(echo $name | sed -e "s/ /\_/g")
-    dirname=$DIR\/$comp\_$pos
-    mkdir $dirname
-    filename=$dirname\/$name\_$comp\_$pos.tex
-    touch $filename
-    cat resume.tex > $filename
+    dirname=$DIR\/$comp\_$pos   # creating the directory that houses the tex document, it is resume_storage/companyname_positionname
+    mkdir $dirname              # make the directory
+    filename=$dirname\/$name\_$comp\_$pos.tex # defining filename which is name_company_position and the a .tex
+    touch $filename                           # create the blank file
+    cat resume.tex > $filename                # copy a blank template of the resume into the newly created file
+
 }
 
-awkFunc(){
+skillCheckerFunc(){             # this function deals with checking if same skill inputted twice
+    
+    skillExist=$(grep $userinp skills | wc -l) # checking the skills the user inputted against skills list (will change later for it is for debugging purposes)
+    if [ "${skillExist}" -eq 0 ];              # if no matches with user input are found in skills list, then input skill into skill list and send message
+    then
+        echo "Inputting: $userinp into skills buffer \n"
+        echo $userinp >> skills 
+    else                        # if there are matches, then that means user inputted skill twice, will make userinp equal to null so
+        printf "Skill: $userinp, already inputted \n"
+        userinp=""
+    fi
+    
+}
+
+mainCondFunc(){     # this function deals with whether or not to run main text editing functions depending on userinput
+    
+    printf "Making tmp file"    # Tmp file and tmp file functions for testing, will later implement
     tmpFile=$(mktemp) || exitFunc
-    awk -v var="$userinp" 'index($0,var)' RS="\n\n" ORS="\n\n" projects_list.tex > buffer.tex 
+
+    if [ -z "$userinp" ];       # if user input is empty, means it was made empty since user inputted same skill, other wise awkFunc
+    then
+        printf "Empty or Repeated Input Please Try Again \n"
+    else
+        awkFunc
+    fi
+    
 }
 
-lvlFunc(){
-    arrayLvl=(A B C)
-    for i in ${arrayLvl[@]}; do
-        lvlVal="Pr:"$i
-        fileVal="buffer"$i".tex"
-        awk -v var="$lvlVal" 'index($0,var)' RS="\n\n" ORS="\n\n" buffer.tex >> $fileVal
+awkFunc(){                      # This is the main function that deals with selecting the projects from list that match with user input
+
+    # using awk to find paragraphs with matching patterns
+    # later inputting them into placehold.tex, NOTE: will change this soon, placehold will not be needed in the future
+    
+    awk -v var="$userinp" 'index($0,var) && index($0,"Skills Used:")' RS="\n\n" ORS="\n\n" projects_list.tex > placehold.tex
+    
+    nameFunc < placehold.tex    # making placehold.tex an input to another function, nameFunc
+    
+    if [ "${boolVar}" -eq "0" ]; # if the boolVar from nameFunc is 0, means no projects of the same name
+    then
+        printf "\n Inputting project into buffer \n"
+        cat placehold.tex > buffer.tex
+        echo $dataVar >> name_list 
+    else
+        printf "\n Project already exists in resume \n"
+    fi
+    # numSkill=$(cat name_list | wc -l) 
+    # printf " \n Number of projects in resume: $numSkill \n"
+    
+}
+
+nameFunc(){                     # This function makes sure that projects of the same name are not inputted into resume
+    
+    while read -r data; do      # while reading data from inputted file (i.e. placehold.tex)
+        dataVar=$(sed -n '/^\\textbf/p' $data | cut -d'{' -f2 | cut -d':' -f1) # extracting names from each project
+        boolVar=$(cat $filename | grep -s $dataVar | wc -l) # searching for projects of same name in resume
+    done
+    
+}
+
+
+lvlFunc(){                      # This function deals organizing priority projects into correct file
+    arrayLvl=(A B C)            # Priority A, B, C
+    for i in ${arrayLvl[@]}; do 
+        lvlVal="Pr:"$i          # Pr:A, Pr:B, Pr:C
+        fileVal="buffer"$i".tex" # bufferA.tex, bufferB.tex, bufferC.tex
+        awk -v var="$lvlVal" 'index($0,var)' RS="\n\n" ORS="\n\n" buffer.tex >> $fileVal # getting priority projects and putting in its corresponding buffer
     done
 }
 
-lvlCleaner(){
+lvlCleaner(){                   # gets rid of the priority tags in the final resume
     sed -i '/Pr:/d' $filename
 }
 
-sedFunc(){
-    arrayLvl=(C B A)
+sedFunc(){                      # this function deals with inputting the accumulated projects from each priority buffer and inputting them into the resume
+    arrayLvl=(C B A)            # reversed since sed inputs below the specified line, therefore C projects will go first, then B above and finally A above 
     for i in ${arrayLvl[@]}; do
         fileVal="buffer"$i".tex"
-        sed -i "/Projects Start/r $fileVal" $filename
-        echo "" > $fileVal
-        done
-        echo "" > buffer.tex
+        printf "Inserting project into file"
+        sed -i "/Projects Start/r $fileVal" $filename # this line inputs them below the line Projects Start in the resume
+        echo "" > $fileVal                            # clear the buffer once done
+    done
+    # Clearing all the buffer files 
+    echo "" > buffer.tex
+    echo "" > skills 
+    echo "" > placehold.tex
+    echo "" > name_list
 }
 
-cleanupFunc(){
-    echo "Adding to $filename file"
-    sedFunc
-     echo "Removing tmp file"
-    rm -f $tmpFile
+cleanupFunc(){                  # Function deals with final process when user done with script
+    echo "Adding to $filename file" 
+    sedFunc                     # Runs the sed function 
+    echo "Removing tmp file"    
+    rm -f $tmpFile              # removes temp file
 
 }
-exitFunc() {
+exitFunc() {                    # exit function for tmp file if it could not be created
     echo "Couldn't make temp file"
     exit 1    
 }
+
 
--- mainScript.sh
+++ mainScript.sh
@@ -5,22 +5,27 @@ trap cleanupFunc EXIT
 
 resStoreCheckFunc
 name="Alvi Ahmed"
-read -p "Enter name of company: " comp
-read -p "Enter name of position: " pos
+comp="Google"
+pos="Engineering"
+# read -p "Enter name of company: " comp
+# read -p "Enter name of position: " pos
 resDirFunc
 while :
 do      
     read -p "Enter Skill: " userinp
+    skillCheckerFunc
     echo ""
-    awkFunc
+    mainCondFunc
     lvlFunc
     numLines=$(grep $userinp buffer.tex | wc -l) 
 
     if [ "${numLines}" -eq "0" ];
     then
         echo "No projects exist for skill: $userinp"
+        echo ""
     else
         echo "Project(s) exist for this skill: $userinp  "
+        echo ""
     fi
 
     echo "To exit script press <CTRL+C>"
